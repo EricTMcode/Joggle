e826a2ef35aa9db5cbdf3f8d51e51173c6d23337
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-08 15:54:13 +0100

    Initial Commit

---
+++ Joggle/JoggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoggleApp.swift
+//  Joggle
+//
+//  Created by Eric on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct JoggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
